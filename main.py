21837cc39bd6ede540ec9db0fac6e2861439dcbe
yogamaaya <motadaka@yahoo.com>
2024-12-07 22:36:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 from message_handler import receive_message
+
 app = Flask(__name__)
 # Simple storage to keep messages in memory
 messages = []
 
+
 @app.route('/')
 def chat():
     return render_template('chat.html')
+
+
 @app.route('/submit', methods=['POST', 'GET'])
 def submit_message():
     new_messages = receive_message()
@@ -14,9 +18,11 @@ def submit_message():
     messages = new_messages  # Update the messages with the latest messages
     return new_messages
 
+
 @app.route('/chat', methods=['GET'])
 def display_chat():
     return render_template('chat.html', messages=messages)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
